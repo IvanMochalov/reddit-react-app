5dd984ffdbccb0312777a1ef483c49c458a6f7aa
Ivan Mochalov <mochalov.ivan2015@mail.ru>
2023-04-13 19:13:24 +0300

    fix

--- src/shared/UserBlock/UserBlock.tsx
+++ src/shared/UserBlock/UserBlock.tsx
@@ -18,7 +18,7 @@ export function UserBlock({ avatarSrc, username, loading, error }: IUserBlockPro
   const { oauth } = useAppSelector((state) => state.userData);
   return (
     <Link
-      to={oauth ? '/account' : `https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&state=random_string&redirect_uri=${process.env.REDIRECT}/auth&duration=permanent&scope=read submit identity`}
+      to={oauth ? '/account' : `https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&state=random_string&redirect_uri=${process.env.REDIRECT}&duration=permanent&scope=read submit identity`}
       className={styles.userBox}>
       <div className={styles.avatarBox}>
         {avatarSrc
